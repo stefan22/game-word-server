dbda727109d82c3a7d2310132822d44b75fd364e
stefano <stefanosemail@protonmail.com>
2021-02-25 19:20:45 +0000

    [test] jest file

---
+++ index.test.js
@@ -0,0 +1,33 @@
+const request = require('supertest');
+const app = require('./index.js');
+const $ = require('jquery');
+
+const PORT = 4000;
+
+let server, agent, response;
+beforeAll((done) => {
+  server = app.listen(PORT, async (err) => {
+    if (err) return done(err);
+    agent = request.agent(server);
+    response = await agent.get('/word');
+    done();
+  });
+});
+
+describe('GET/word', () => {
+  test('server responds with status 200', async () => {
+    expect.assertions(1);
+    await expect(response.statusCode).toBe(200);
+  });
+
+  test('response should contain heading "Guess Word"', async () => {
+    const txtData = $(response.text).text();
+    await expect(txtData).toContain('Guess Word');
+  });
+
+  test('response should be a five-letter word', async () => {
+    document.documentElement.innerHTML = response.text;
+    let word = $('#output').text().trim();
+    await expect(word.length).toBe(5);
+  });
+});
